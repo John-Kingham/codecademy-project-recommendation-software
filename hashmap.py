99f38f84e65fa0b4685657ff4d73cdcf1b43dd00
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-02-19 21:03:15 +0000

    Updated hashmap.add() to remove duplicate code.

--- hashmap.py
+++ hashmap.py
@@ -5,7 +5,7 @@ class HashMap:
 
     def __init__(self, size: int = 100) -> None:
         self.size: int = size
-        self.map: list[None | list[object]] = [None] * size
+        self.map: list[list[object]] = [[]] * size
 
     def add(self, key: str, value: object) -> bool:
         """
@@ -16,33 +16,15 @@ class HashMap:
         True if the value was added or updated.
         False if the hashmap was full.
         """
-
-        # try to add or update the key-value pair
-
-        key_index: int = 0
-        value_index: int = 1
-        index: int = self._hash(key)
-        key_value_pair: None | list[object] = self.map[index]
-        if key_value_pair is None:
-            self.map[index] = [key, value]
-            return True
-        if key_value_pair[key_index] == key: # type: ignore
-            self.map[index][value_index] = value # type: ignore
-            return True
-
-        # if there was a collision, repeatedly try another index
-
-        num_collisions: int = 1
-        # TODO: I DON'T THINK THE WHILE TRUTH VALUE IS RIGHT. 
-        # WE'RE LEAVING THE LOOP WHEN WE SUCCEED, SO CAN'T WE JUST SAY WHILE TRUE?
-        while key_value_pair[key_index] != key: # type: ignore
-            index = self._hash(key, num_collisions)
-            key_value_pair = self.map[index]
-            if key_value_pair is None:
+        num_collisions: int = 0
+        while num_collisions < self.size:
+            index: int = self._hash(key, num_collisions)
+            key_and_value: list[object] = self.map[index]
+            if not key_and_value:
                 self.map[index] = [key, value]
                 return True
-            if key_value_pair[key_index] == key:
-                key_value_pair[value_index] = value
+            if key_and_value[0] == key:
+                self.map[index][1] = value
                 return True
             num_collisions += 1
         return False
@@ -59,8 +41,9 @@ class HashMap:
             If the key isn't found.
         """
         index: int = self._hash(key)
-        if self.map[index]:
-            return self.map[index][1]  # type: ignore
+        key_and_value: list[object] = self.map[index]
+        if key_and_value:
+            return key_and_value[1]
         return None
 
     def _hash(self, key: str, num_collisions: int = 0) -> int:
